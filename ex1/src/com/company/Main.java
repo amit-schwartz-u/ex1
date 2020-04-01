cba03c7bd7b32a46001e0a05dc70970d9faea4d6
amit-schwartz-u <57283327+amit-schwartz-u@users.noreply.github.com>
2020-04-01 10:14:23 +0300

    d

--- ex1/src/com/company/Main.java
+++ ex1/src/com/company/Main.java
@@ -3,5 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("amit");}
+        System.out.println("amit");
+        System.out.println("amit");
+    }
 }
